6eaa0faca502ccf456221874782c824de133275c
IamRoshanAc <roshanac6533@gmail.com>
2024-08-17 13:56:30 +0545

    audit-8

--- dinevue-frontend/src/components/security.jsx
+++ dinevue-frontend/src/components/security.jsx
@@ -15,16 +15,7 @@ const Security = () => {
     <div className="search-bar-container">
       <h1 className="title">Let's find your ideal table</h1>
       <div className="search-row">
-        <div className="search-container">
-          <FaSearch className="search-icon" />
-          <input 
-            type="text" 
-            className="searchbar-input" 
-            placeholder="Search Restaurant" 
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
+        
         <button className="search-button" onClick={handleSearch}>Search</button>
       </div>
     </div>
