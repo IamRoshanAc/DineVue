8c282c67647cb328368d6584e93ddfeb43f27d0f
IamRoshanAc <roshanac6533@gmail.com>
2025-06-08 13:02:56 +0545

    ..

--- dinevue-backend/model/userModel.js
+++ dinevue-backend/model/userModel.js
@@ -48,7 +48,15 @@ const User = sequelize.define('User', {
   savedRestaurants: {
     type: DataTypes.ARRAY(DataTypes.INTEGER), // Array of integers (restaurant IDs)
     defaultValue: [],
-  }
+  },
+  loginAttempts: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  lockUntil: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
 }, {
   tableName: 'users',
 });
